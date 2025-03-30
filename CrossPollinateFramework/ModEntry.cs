0f230ee5c8f64cf4e20f2a82d1f4de281d35bf08
Austin McGowan <a.r.mcgowan@gmail.com>
2025-03-30 12:53:09 -0500

    it starts

---
+++ CrossPollinateFramework/ModEntry.cs
@@ -0,0 +1,19 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace CrossPollinateFramework
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+             
+        }
+    }
+}
+
